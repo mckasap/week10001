22ffbe16a94bfe8678379f0015be331141c845d2
mckasap <mckasap@gmail.com>
2018-05-03 10:37:31 +0300

    Web view eklendi

--- app/src/main/java/org/kasapbasi/week10001/MainActivity.java
+++ app/src/main/java/org/kasapbasi/week10001/MainActivity.java
@@ -5,14 +5,18 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    WebView wv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        wv= (WebView) findViewById(R.id.myWebView);
+
     }
 
     public void OpenAlert(View v){
@@ -24,10 +28,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 Toast.makeText(MainActivity.this," THANKS BRO ",Toast.LENGTH_SHORT).show();
-
+             wv.loadUrl("http://www.ticaret.edu.tr");
             }
         });
 
+
+        builder.setNeutralButton("Nötr", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this," Nötr THANKS BRO ",Toast.LENGTH_SHORT).show();
+
+            }
+        });
        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
            @Override
            public void onClick(DialogInterface dialogInterface, int i) {
